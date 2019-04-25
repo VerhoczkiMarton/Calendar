525fe6ea9db8e8b45e103be8c0d37fdc9138ec2d
verho <verhoczki.marci@gmail.com>
2019-04-25 12:46:51 +0200

    Add get_inputs to ui

--- ui.py
+++ ui.py
@@ -6,3 +6,25 @@ def print_menu(menu_options):
     for option in menu_options:
         print(f'({option[0]}) {option}')
     print('')
+
+
+def get_inputs(titles):
+    if type(titles) == str:
+        user_input = input(f'{titles}: ')
+        try:
+            user_input = int(user_input)
+        except:
+            pass
+        finally:
+            return user_input
+    else:
+        inputs = []
+        for title in titles:
+            user_input = input(f'{title}: ')
+            try:
+                user_input = int(user_input)
+            except:
+                pass
+            finally:
+                inputs.append(user_input)
+        return inputs
