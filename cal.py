90b82b760497865d18da6642159786bac84cf447
verho <verhoczki.marci@gmail.com>
2019-04-26 13:29:31 +0200

    Implement time validating

--- cal.py
+++ cal.py
@@ -23,19 +23,13 @@ def main():
 
 def add_meeting(schedule):
     ui.print_message('Schedule a new meeting.')
-    meeting = [[], [], []]
-    valid_durations = [1, 2]
-    valid_starts = [x for x in range(25)]
-
-    while (meeting[index('duration')] not in valid_durations) or (meeting[index('start')] not in valid_starts):
+    meeting = ui.get_inputs(['Enter meeting title',
+                                 'Enter duration in hours (1 or 2)',
+                                 'Enter start time'])
+    while not validate_meeting(schedule, meeting):
         meeting = ui.get_inputs(['Enter meeting title',
                                  'Enter duration in hours (1 or 2)',
                                  'Enter start time'])
-        if (meeting[index('duration')] not in valid_durations):
-            ui.print_message('Invalid duration.')
-        elif (meeting[index('start')] not in valid_starts):
-            ui.print_message('Invalid start time.')
-
     ui.print_message('Meeting added.')
     schedule.append(meeting)
     return schedule
@@ -62,6 +56,44 @@ def index(name):
         return 2
     else:
         raise IndexError
+
+
+def validate_meeting(schedule, meeting):
+    valid_durations = [1, 2]
+    valid_starts = [x for x in range(8, 19)]
+    meeting_times_current = get_meeting_times(meeting)
+    meeting_times_already = [get_meeting_times(x) for x in schedule]
+    overlap = False
+
+    for meeting_time_already in meeting_times_already:
+        overlap = get_overlap(meeting_time_already, meeting_times_current)
+
+    if meeting[index('duration')] not in valid_durations:
+        ui.print_message('ERROR: Invalid duration.')
+        return False
+    elif meeting[index('start')] not in valid_starts:
+        ui.print_message('ERROR: Meeting is outside of your working hours (8 to 18)!')
+        return False
+    elif overlap:
+        ui.print_message('ERROR: Meeting overlaps with existing meeting!')
+        return False
+    else:
+        return True
+
+
+def get_meeting_times(meeting):
+    meeting_times = []
+    meeting_times.append(meeting[index('start')])
+    for i in range(meeting[index('duration')]):
+        meeting_times.append(meeting_times[i] + 1)
+    return meeting_times
+
+
+def get_overlap(meeting_times_1, meeting_times_2):
+    for meeting_time_1 in meeting_times_1:
+        if meeting_time_1 in meeting_times_2:
+            return True
+    return False
 
 
 if __name__ == "__main__":
